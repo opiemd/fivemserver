f746dee553ee5b51ffbb5f002fbc32733089514d
OpieMD <deviltrymd@gmail.com>
2019-10-03 16:43:29 +0200

    essential

---
+++ resources/esx_robnpc/__resource.lua
@@ -0,0 +1,20 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Rob NPC'
+
+server_scripts {
+     'config.lua',
+     'server/main.lua'
+}
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'locales/en.lua',
+	'locales/de.lua',
+    'config.lua',
+    'client/main.lua'
+}
+
+dependencies {
+    'es_extended'
+}
---
+++ resources/esx_robnpc/locales/de.lua
@@ -0,0 +1,8 @@
+Locales['de'] = {
+    ['robbed_too_recently'] = "Du hast zu kürzlich ausgeraubt.",
+    ['target_dead']         = "Die person die du ausraben möchtest, ist tot.",
+    ['target_too_far']      = "Ziel ist zu weit weg.",
+    ['robbery_started']     = "Raub findet gerade statt!",
+    ['robbery_completed']   = "Du hast ~g~$%s~s~!",
+    ['can_rob_again']       = "Du kannst wieder ausrauben!",
+}
